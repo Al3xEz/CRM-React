bd0c36826a6a4a31f53809af2f389386680db227
Jorge Solano Cordero <alexis12115@gmail.com>
2022-10-16 06:51:10 -0600

    Instalación de Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
   return (
-    <h1>Base</h1>
-  )
+    <>
+      <h1 className="text-6xl font-bold">Base</h1>
+    </>
+  );
 }
 
-export default App
+export default App;
